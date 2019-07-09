ae1668b069b72942e114f2c5d0fb9efde7b30813
snowandbamboo <1215134782@qq.com>
2019-07-09 19:35:46 +0800

    commit11

--- src/main/java/com/atguigu/Hello.java
+++ src/main/java/com/atguigu/Hello.java
@@ -6,8 +6,8 @@ public class Hello {
         System.out.println("分久必合，合久必分");
         System.out.println("---------------");
         System.out.println("海贼王");
-        System.out.println("你是一个超级备胎")
-        System.out.peintlm("你是个备胎")
+        System.out.println("你是一个超级备胎");
+        System.out.println("你是个备胎");
         System.out.println("00000000000000000000");
         System.out.println("你是个备胎");
         System.out.println("fork");
